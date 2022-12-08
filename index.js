c7bdcd8aa0047542185ca0c2a4c83c3a766b4273
brandon <brandon14ogola@gmail.com>
2022-12-08 17:34:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,59 +1,35 @@
 // Code your solution in this file!
-describe('index.js', function() {
-    function distanceFromHqInBlocks(someValue) {
-        expect(distanceFromHqInBlocks(43)).to.equal(1);
-    };
-    it('returns a distance in blocks', function) {
-        expect(distanceFromHqInBlocks(50)).to.equal(8);
-    };
-    it('calculate distance below 42nd street', funtion()) {
-        expect(distanceFromHqInBlocks(34)).to.equal(8);
-    });
-});
-describe('distanceFromHqInFeet()', function() {
-    it('returns a distance in feet', function() {
-        expect(distanceFromHqInFeet(43)).to.equal(264);
-    });
-
-    it('returns a distance in feet', function() {
-        expect(distanceFromHqInFeet(50)).to.equal(2112);
-    });
-
-    it('calculates distances below 42nd street', function() {
-        expect(distanceFromHqInFeet(34)).to.equal(2112);
-    });
-});
-
-describe('distanceTravelledInFeet()', function() {
-    it('returns the distance travelled in feet', function() {
-        expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
-    });
-
-    it('returns a distance in feet', function() {
-        expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
-    });
-
-    it('returns distance when destination is below distance', function() {
-        expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
-    });
-});
-
-describe('calculatesFarePrice(start, destination)', function() {
-it('gives customers a free sample', function() {
-    expect(calculatesFarePrice(43, 44)).to.equal(0);
-});
-
-it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-    // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
-    expect(calculatesFarePrice(34, 32)).to.equal(2.56);
-});
-
-it('charges 25 dollars for a distance over 2000 feet', function() {
-    expect(calculatesFarePrice(50, 58)).to.equal(25);
-});
-
-it('does not allow rides over 2500 feet', function() {
-    expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
-});
-});
-});
+function distanceFromHqInBlocks (blockNumber) {
+    if (blockNumber > 42) {
+      return blockNumber - 42;
+    } else {
+      return 42 - blockNumber;
+    }
+  }
+function distanceFromHqInFeet (blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+  }
+  
+  function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264;
+    } else {
+      return (start - destination) * 264;
+    }
+  }
+  
+  function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  Footer
+  
